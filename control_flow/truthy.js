d2d5be411c3d82bb6016f17cbcca6e4c2c30571e
kinjal1233 <kinjalvaghela754@gmail.com>
2025-01-22 10:34:01 +0000

    controlflow

---
+++ control_flow/truthy.js
@@ -0,0 +1,48 @@
+/* truth or falsy value
+
+falsy --- false, 0, -0, bigInt 0n, "empty string", null, undefined, Nan 
+
+truthy --- "0", 'false', " ", [], {}, function(){}
+
+
+*/
+
+
+
+// const useremail = []
+
+// if (useremail) {
+//      console.log("got email");
+// } else {
+//      console.log("didn't get");
+// }
+
+// if (useremail.lenght === 0) {
+//     console.log("array is empty");
+// }
+
+
+// const emptyobj = {}
+// if (Object.keys(emptyobj). lenth === 0) {
+// console.log("object is empty")
+// }
+
+
+
+// nullish coalescing operator (??): null, undefined
+
+let val1;
+//  val1 = 5 ?? 10
+//  val1 = null ?? 10
+//  val1 = undefined ?? 10
+// console.log(val1);
+
+
+
+// ++++++++++++++++++  terniary operator +++++++++++++++++++++
+
+// condition ? true : false
+
+const iceTeaPrice = 100 
+iceTeaPrice <= 80 ? console.log("less then 80") : 
+console.log("more then 80")
